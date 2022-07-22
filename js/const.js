0509e61c75b48b61f6590e31c6e2c0b6bdcba518
iwtts <berzinao@hotmail.com>
2022-07-22 12:53:13 +0300

    add address marker and adform validation

--- js/const.js
+++ js/const.js
@@ -2,6 +2,10 @@ const GET_DATA_URL = 'https://23.javascript.pages.academy/keksobooking/data';
 const POST_DATA_URL = 'https://23.javascript.pages.academy/keksobooking';
 const INITIAL_MAP_ZOOM = 13;
 const ADS_MAX = 10;
+const LOCATION_DECIMAL = 5;
+const TITLE_LENGTH_MAX = 100;
+const TITLE_LENGTH_MIN = 30;
+const PRICE_MAX = 1000000;
 
 const INITIAL_MAP_VIEW = {
   lat: 35.6894,
@@ -9,9 +13,15 @@ const INITIAL_MAP_VIEW = {
 };
 
 const MARKER_ICON = L.icon({
-  iconUrl: '../img/map-pins/pin.svg',
+  iconUrl: '../img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
+});
+
+const MAIN_MARKER_ICON = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [50, 50],
+  iconAnchor: [25, 50],
 });
 
 export {
@@ -19,6 +29,11 @@ export {
   POST_DATA_URL,
   INITIAL_MAP_ZOOM,
   ADS_MAX,
+  LOCATION_DECIMAL,
+  TITLE_LENGTH_MAX,
+  TITLE_LENGTH_MIN,
+  PRICE_MAX,
   INITIAL_MAP_VIEW,
-  MARKER_ICON
+  MARKER_ICON,
+  MAIN_MARKER_ICON
 };
